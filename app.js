52478e07621041f9a1c776defda7f01034287165
Kryptöñ <madhusudanbabar@gmail.com>
2021-11-25 15:37:43 +0530

    favourites done

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-document.onload = function () {
-  let img = new Image();
-  img.src = "https://source.unsplash.com/1920x1080/?cat";
-  img.onload = function (e) {
-    console.log("image loaded");
-    console.log(e);
-  };
-};
+var imgDetails;
 
 window.addEventListener("load", () => {
-  let img = new Image();
-  img.src = "https://source.unsplash.com/1920x1080/?cat";
-  img.onload = function (e) {
-    console.log("image loaded");
-    // console.log(e);
-  };
+  // console.log("img", img);
 
   fetch("https://source.unsplash.com/1920x1080/?cat").then((res) => {
     console.log(res);
+
+    let img = new Image();
+    img.src = res.url;
+    imgDetails = res;
+    img.onload = function (e) {
+      console.log("image loaded");
+      console.log(e);
+    };
+    img.setAttribute("class", "tab__img");
+    let img_placeholder = document.querySelector(".tab__pic");
+    img_placeholder.appendChild(img);
   });
 });
 
@@ -24,10 +23,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let btn_heart = document.querySelector(".tab__icon--heart");
   let btn_shuffle = document.querySelector(".tab__icon--shuffle");
   let btn_menu = document.querySelector(".tab__icon--menu");
+  let btn_debug = document.querySelector(".tab__icon--debug");
 
   btn_heart.addEventListener("click", () => {
-    console.log("heart clicked");
-    // todo
+    let localCats = [];
+    // get local cats
+    localCats = JSON.parse(localStorage.getItem("favKats"));
+    localCats
+      ? localStorage.setItem(
+          "favKats",
+          JSON.stringify([...localCats, imgDetails.url])
+        )
+      : localStorage.setItem("favKats", JSON.stringify([imgDetails.url]));
+
+    console.log("localCats", localCats);
   });
 
   btn_shuffle.addEventListener("click", () => {
@@ -39,4 +48,21 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("menu clicked");
     // todo
   });
+
+  btn_debug.addEventListener("click", () => {
+    imgDetails ? console.log(imgDetails) : console.log("Sed Lyf! no img");
+    // todo
+  });
 });
+
+// Obsolete code
+
+// document.onload = function () {
+//   let img = new Image();
+//   img.src = "https://source.unsplash.com/1920x1080/?cat";
+//   img.onload = function (e) {
+//     console.log("image loaded");
+//     console.log(e);
+//   };
+//   console.log("img", img);
+// };
--- scratchpad.md
+++ scratchpad.md
@@ -27,3 +27,12 @@
 # In Progress
 
 - favourite Kats
+
+# Scratchpad
+
+- url doesn't actually expands in the src attribute,
+  so I need to figure out the URL after redirection, so for that purpose, I'll need to use fetch API instead of directly using the URL, so that I can use later keep track of favourites and un-favourites.
+
+# WIP
+
+- favourite Kat
